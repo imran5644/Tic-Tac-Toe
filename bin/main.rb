535ce81fb8ff1065dd81c2d87b978e7f09eb135f
imran5644 <imi56444@gmail.com>
2020-11-16 21:10:44 +0530

    removed linters errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,22 +1,22 @@
 #!/usr/bin/env ruby
-puts "Welcome to Tic Tac Toe Game"
+puts 'Welcome to Tic Tac Toe Game'
 
-puts" please enter the name of player1"
+puts 'please enter the name of player1'
 player1 = gets.chomp
 
-puts" please enter the name of player2"
+puts 'please enter the name of player2'
 player2 = gets.chomp
 
-puts "the game is playing by #{player1} and #{player2}"
+puts 'the game is playing by #{player1} and #{player2}'
 
 puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 def game_board(board)
-   puts row = " #{board[0]} |#{board[1]} |  #{board[2]}"
-   puts col = "--------------------"
-   puts row = " #{board[3]} | #{board[4]}| #{board[5]}"
-   puts col = "--------------------"
-   puts row = " #{board[6]} | #{board[7]}|#{board[8]} "  
+   puts row = "#{board[0]}|#{board[1]}|#{board[2]}"
+   puts col = '-------------------'
+   puts row = "#{board[3]}|#{board[4]}|#{board[5]}"
+   puts col = '--------------------'
+   puts row = "#{board[6]}| #{board[7]}|#{board[8]}"  
 end
 
 game_board(board)
