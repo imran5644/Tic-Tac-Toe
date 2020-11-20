ae32a76a01c608ca8173c84f7078d49373f380cb
imran5644 <imi56444@gmail.com>
2020-11-21 01:16:18 +0530

    removed rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/env ruby
 # rubocop:disable Metrics/BlockLength
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
-require_relative "../lib/game.rb"
+require_relative '../lib/game'
 game = Tictactoe.new
 loop do
-  system("clear")
+  system('clear')
   puts 'Welcome to Tic Tac Toe Game'
   puts 'please enter the name of player1'
   playerone = gets.chomp
@@ -15,7 +12,7 @@ loop do
   gameon = true
   counts = 0
   playerturn = 'x'
-  board = [ 1, 2, 3, 4, 5, 6 , 7, 8, 9]
+  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   puts game.game_board(board)
   while gameon
     puts "the game is playing by #{playerone} and #{playertwo}"
@@ -26,12 +23,12 @@ loop do
     puts 'enter number between 1 and 9'
     user_input = gets.chomp
     puts "player chose #{user_input}"
-    if !game.valid_move?(board, (user_input))
+    if !game.valid_move?(board, user_input)
       puts 'this is not a valid move'
     else
       puts 'this is a valid move'
       counts += 1
-      game.players_move((user_input), board, playerturn)
+      game.players_move(user_input, board, playerturn)
       break if game.won?(board, playerturn) || game.draw?(board)
 
       if playerturn == 'x'
@@ -41,7 +38,7 @@ loop do
         puts playerone.to_s
         playerturn = 'x'
       end
-    
+
     end
     puts game.game_board(board)
     gameon = false if counts == 9
@@ -49,11 +46,8 @@ loop do
   puts game.game_board(board)
   puts "#{playerturn} won " if game.won?(board, playerturn)
   puts 'all positions filled and it\'s a draw' if game.draw?(board)
-  puts 'do you want to play again? yes or no' 
+  puts 'do you want to play again? yes or no'
   wanna_play = gets.chomp
   break if wanna_play == 'no'
 end
 # rubocop:enable Metrics/BlockLength
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/PerceivedComplexity
